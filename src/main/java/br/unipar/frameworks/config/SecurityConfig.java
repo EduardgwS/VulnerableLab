2d5363794a78ce654b752cbbe1983e883f3ccae0
eduardgw <eduard.gw2006@hotmail.com>
2026-05-25 19:44:41 -0300

    Realização da Fase 4

--- src/main/java/br/unipar/frameworks/config/SecurityConfig.java
+++ src/main/java/br/unipar/frameworks/config/SecurityConfig.java
@@ -8,16 +8,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import br.unipar.frameworks.config.RateLimitFilter;
 
 @EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           JwtAuthFilter jwtAuthFilter,
+                                           RateLimitFilter rateLimitFilter) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+                .addFilterBefore(rateLimitFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
--- src/main/java/br/unipar/frameworks/controller/ProductController.java
+++ src/main/java/br/unipar/frameworks/controller/ProductController.java
@@ -3,6 +3,9 @@ package br.unipar.frameworks.controller;
 import br.unipar.frameworks.model.Product;
 import br.unipar.frameworks.repository.ProductRepository;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -17,8 +20,14 @@ public class ProductController {
     }
 
     @GetMapping
-    public List<Product> listProducts() {
-        return productRepository.findAll();
+    public Page<Product> listProducts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+
+        Pageable pageable = PageRequest.of(page, size);
+
+        return productRepository.findAll(pageable);
     }
 
     @PostMapping
